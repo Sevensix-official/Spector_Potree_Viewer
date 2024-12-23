f36dc31a41d5f36e93d4ce4858c6a1c4b9430334
S.Gunji <gunji@sevensix.co.jp>
2024-12-23 18:33:36 +0900

    initialize

---
+++ point_cloud_converted/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 635491,
+    "boundingBox": {
+        "lx": 16.091474533081056,
+        "ly": 84.49700164794922,
+        "lz": 0.1,
+        "ux": 199.55947453308108,
+        "uy": 267.96500164794926,
+        "uz": 183.568
+    },
+    "tightBoundingBox": {
+        "lx": 16.091474533081056,
+        "ly": 84.49700164794922,
+        "lz": 0.1,
+        "ux": 115.42547453308106,
+        "uy": 136.7260016479492,
+        "uz": 183.568
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.5888794660568238,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
